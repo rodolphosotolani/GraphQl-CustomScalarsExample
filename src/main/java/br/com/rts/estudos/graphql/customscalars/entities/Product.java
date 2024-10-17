ebfec495c0be9abb1b65468036d66458bb810a80
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-17 00:14:14 -0400

    feat: Create a new Product entity

---
+++ src/main/java/br/com/rts/estudos/graphql/customscalars/entities/Product.java
@@ -0,0 +1,23 @@
+package br.com.rts.estudos.graphql.customscalars.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String title;
+
+}
