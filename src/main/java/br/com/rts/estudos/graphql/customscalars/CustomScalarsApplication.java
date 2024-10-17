818850e51891faa143feb791ef05dfb89cc4b4ce
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-17 00:05:59 -0400

    feat: Create a new project

---
+++ src/main/java/br/com/rts/estudos/graphql/customscalars/CustomScalarsApplication.java
@@ -0,0 +1,13 @@
+package br.com.rts.estudos.graphql.customscalars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomScalarsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomScalarsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/rts/estudos/graphql/customscalars/CustomScalarsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rts.estudos.graphql.customscalars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomScalarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
