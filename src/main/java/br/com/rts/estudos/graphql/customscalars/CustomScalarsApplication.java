39c42925103b201f59dcab9c55ebf943ebf35522
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-17 00:15:35 -0400

    feat: populate Product in database with commandLineRunner.

--- src/main/java/br/com/rts/estudos/graphql/customscalars/CustomScalarsApplication.java
+++ src/main/java/br/com/rts/estudos/graphql/customscalars/CustomScalarsApplication.java
@@ -1,13 +1,39 @@
 package br.com.rts.estudos.graphql.customscalars;
 
+import br.com.rts.estudos.graphql.customscalars.entities.Product;
+import br.com.rts.estudos.graphql.customscalars.repositories.ProductRepository;
+import net.datafaker.Faker;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
 public class CustomScalarsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomScalarsApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(Faker faker, ProductRepository repository) {
+        return args -> {
+            List<Product> products = List.of(Product.builder().title(faker.commerce().productName()).build(),
+                    Product.builder().title(faker.commerce().productName()).build(),
+                    Product.builder().title(faker.commerce().productName()).build(),
+                    Product.builder().title(faker.commerce().productName()).build(),
+                    Product.builder().title(faker.commerce().productName()).build(),
+                    Product.builder().title(faker.commerce().productName()).build());
+
+            repository.saveAll(products);
+        };
+    }
+
+    @Bean
+    public Faker getFaker() {
+        return new Faker();
     }
 
 }
