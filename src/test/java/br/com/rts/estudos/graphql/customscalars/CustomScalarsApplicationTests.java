9189bdac75645f486f8009a6b0221c78cd5f54bb
Rodolpho T. Sotolani <rt.sotolani@gmail.com>
2024-10-17 00:16:39 -0400

    feat: comment the unit test

--- src/test/java/br/com/rts/estudos/graphql/customscalars/CustomScalarsApplicationTests.java
+++ src/test/java/br/com/rts/estudos/graphql/customscalars/CustomScalarsApplicationTests.java
@@ -1,13 +1,12 @@
 package br.com.rts.estudos.graphql.customscalars;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CustomScalarsApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }
